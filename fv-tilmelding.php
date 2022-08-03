20afb8c1422058daf49e3112f2f64943c5fe3d1d
Mikkel Westh <westh_mikkel@hotmail.com>
2022-08-03 13:48:32 +0000

    REWORKED main file into a class

--- fv-tilmelding.php
+++ fv-tilmelding.php
@@ -10,20 +10,49 @@ Author URI: https://westh.it
 
 include "fv-tilmelding-options.php";
 
-FVTilmeldingOptions::init();
+add_action( 'init', 'FVTilmelding::init' );
+class FVTilmelding {
+  static $current_slug;
 
-add_action('parse_request', 'fv_tilmelding_parse_request');
-function fv_tilmelding_parse_request($wp) {
-  // echo "<pre>";
-  // var_dump($wp);
-  // echo "</pre>";
+  static function init() {
+    FVTilmeldingOptions::init();
+    add_action('pre_get_posts', 'FVTilmelding::pre_get_posts');
+  }
 
-  if (FVTilmeldingOptions::is_enabled_page($wp->request)) {
-    $wp->query_vars = [
-      "pagename" => FVTilmeldingOptions::default_page()
-    ];
+  static function pre_get_posts(&$query) {
+    // Only run on main query
+    if (!$query->is_main_query()) return;
+    //error_log("Query:".print_r($query, true), 3, plugin_dir_path(__FILE__)."/debug.log");
+
+    // Check if we're trying to access an enabled signup page
+    if (!FVTilmeldingOptions::is_enabled_page($query->query['name'])) return;
+
+    // Save page adress for later
+    self::$current_slug = $query->query['name'];
+    // Go to the signup page
+    $query->query_vars['name'] = FVTilmeldingOptions::default_page();
+    
+    // Variables needed for correct template
+    $query->is_page = 1;
+    $query->is_single = '';
+
+    // Correct the url for WMPL language selector
+    add_filter('wpml_ls_language_url', function($url) {
+      // Replace slug of default page with slug of actual page
+      return str_replace(FVTilmeldingOptions::default_page(), self::$current_slug, $url);
+    });
+
+    // Change page content so people know the signup is available
+    add_filter('the_content', function($content) {
+      $content = 
+        "<h1>Fastaval Signup</h1>".
+        "<h2>Loading signup plugin</h2>".
+        "<p>If this message doesn't go away, there might have been a script error.<br>".
+        "You may need to use a different browser and/or make sure javascript is enabled</p>"
+      ;
+      return $content;
+    });
 
     //TODO add scripts to page
   }
 }
-
