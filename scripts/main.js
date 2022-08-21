de79438c2b7d164eb6ab92208970e7cefb274fb1
Mikkel Westh <westh_mikkel@hotmail.com>
2022-08-21 13:39:32 +0000

    ADDED Placholder script

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,10 @@
 "using strict";
+jQuery(function($) {
+    let main_content = $(".post-content");
+    let placeholder = $(".signup-placeholder");
+    placeholder.remove();
+    main_content.append(`
+    <h2>Plugin loaded<h2>
+    <p>plugin script has been successfully loaded.</p>
+    `)
+});
